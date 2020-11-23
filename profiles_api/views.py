257dd0b1d39bc046d8e94c0496f6c1bb6a2a8c88
FancyDog <grustnayasova@gmail.com>
2020-11-23 05:53:44 +0300

    work with urls and get request

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class HelloApiView(APIView):
+    """Test API VIEW"""
+
+    def get(self, request, format=None):
+        """Returns a list of APIView features"""
+
+        an_apiview = [
+            '1111111111111111111111',
+            '2222222222222222222222',
+            '3333333333333333333333',
+            'sosi huy pidrila',
+        ]
+
+        return Response({'message': 'Hello', 'an_apiview' : an_apiview})
